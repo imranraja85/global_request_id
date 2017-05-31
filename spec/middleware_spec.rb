a7a8433a7579045d17dfd91583133aa80b65cddb
Imran Raja <imranraja85@gmail.com>
2017-05-30 23:35:33 -0400

    tes tupdate

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -2,16 +2,21 @@ require "spec_helper"
 
 RSpec.describe GlobalRequestId::Middleware do
   let(:app) { ->(env) { [200, env, "app"] } }
-
   let(:middleware) { described_class.new(app) }
-
   let(:request_id) { "abc123" }
-
   let(:env) { { 'action_dispatch.request_id' => request_id } }
 
-  it 'sets the reqest id' do
-    middleware.call(env)
+  describe '#call' do
+    context 'within a request id set by action dispatch' do
+      it 'returns the code, headers and body' do
+        expect(middleware.call(env)).to eq([200, env, "app"])
+      end
 
-    expect(GlobalRequestId.get).to eq(request_id)
+      it 'sets the request id' do
+        middleware.call(env)
+
+        expect(GlobalRequestId.get).to eq(request_id)
+      end
+    end
   end
 end
